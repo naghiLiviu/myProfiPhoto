8cfba705317dbbfc21d9e60863a523f15daee41b
Shaquille92 <slobodan.ned@gmail.com>
2015-12-21 13:46:38 +0200

    added Module & autoload_classmap

---
+++ module/myProfiPhoto/Module.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slobodan
+ * Date: 12/21/15
+ * Time: 10:49 AM
+ */
+
+namespace Album;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+}
---
+++ module/myProfiPhoto/autoload_classmap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slobodan
+ * Date: 12/21/15
+ * Time: 1:39 PM
+ */
+
+return array();
