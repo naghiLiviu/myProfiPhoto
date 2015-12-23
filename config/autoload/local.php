28e1abbd1b34530f9fb3ddd9e04d5b9c02b7989e
Shaquille92 <slobodan.ned@gmail.com>
2015-12-23 15:31:06 +0200

    erased .gitignore with local and added local.php

---
+++ config/autoload/local.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slobodan
+ * Date: 12/21/15
+ * Time: 4:15 PM
+ */
+
+return array(
+    'db' => array(
+        'username' => 'root',
+        'password' => 'root',
+    ),
+);
